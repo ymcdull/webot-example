3141215a26f155c172edde6608b5720c3a3849fb
zhuyunfeng <yunfeng.frank.zhu@gmail.com>
2013-11-10 13:34:40 +0800

    添加url.php

---
+++ url.php
@@ -0,0 +1,36 @@
+<?php  
+define ( "TOKEN", "ymcdull" );  
+$wechatObj = new wechatCallbackapiTest();  
+$wechatObj->valid();  
+  
+class wechatCallbackapiTest  
+{  
+    public function valid()  
+    {  
+        $echoStr = $_GET["echostr"];        //随机字符串  
+        if($this->checkSignature()){  
+            echo $echoStr;  
+            exit;  
+        }  
+    }  
+    private function checkSignature() {  
+      $signature = $_GET ["signature"];  
+      $timestamp = $_GET ["timestamp"];  
+      $nonce = $_GET ["nonce"];  
+      $token = TOKEN;  
+      $tmpArr = array (  
+            $token,  
+            $timestamp,  
+            $nonce  
+      );  
+      sort ( $tmpArr );  
+      $tmpStr = implode ( $tmpArr );  
+      $tmpStr = sha1 ( $tmpStr );  
+        
+      if ($tmpStr == $signature) {  
+        return true;  
+      } else {  
+        return false;  
+      }  
+    }     
+}    
