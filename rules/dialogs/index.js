aca252969e7aa096d114017378ee9168c36972d6
zhuyunfeng <yunfeng.frank.zhu@gmail.com>
2013-11-13 01:55:40 +0800

    yaml

---
+++ rules/dialogs/index.js
@@ -0,0 +1,9 @@
+module.exports = function(webot) {
+
+webot.dialog(
+  'lonely.yaml', 'gags.yaml', 'greetings.js', 'love.yaml',
+  'praise.yaml', 'bad.yaml', 'sad.yaml',
+  'flirt.yaml', 'emoji.yaml', 'short.yaml'
+);
+
+};
